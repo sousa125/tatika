ff17968397e9521a31255fda99170f334dfc46bc
Sousa <jose.sousa1251@gmail.com>
2020-06-05 00:06:54 -0300

    Corrigindo erro de digitação

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def upload_csv(acao, abertura):
     df = msft.history()
     #df = pd.read_csv(acao+'.csv', decimal=',')
     df['UP'] = (df['High'] - df['Open'])*100/df['Open']
-    df['DOWN'] = (df['Open'] - df['Lor'])*100/df['Open']
+    df['DOWN'] = (df['Open'] - df['Low'])*100/df['Open']
     ABERTURA = float(abertura)
     
     ordem_v = ('{"ordem": "venda",')+('"75%": "R${}",'.format((ABERTURA*(1 + df.describe()['UP']['25%']/100)).round(2)))+('"50%": "R${}",'.format((ABERTURA*(1 + df.describe()['UP']['50%']/100)).round(2)))+('"25%": "R${}"'.format((ABERTURA*(1 + df.describe()['UP']['75%']/100)).round(2)))+"}"
