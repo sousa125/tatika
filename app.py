24ac6406299b6fa60a74c415f5cbf0d37ddfab88
Sousa <jose.sousa1251@gmail.com>
2020-06-05 00:04:32 -0300

    Adicionando API na rota up-csv

--- app.py
+++ app.py
@@ -25,11 +25,11 @@ def index():
 @cross_origin()
 def upload_csv(acao, abertura):
     
-
-
-    df = pd.read_csv(acao+'.csv', decimal=',')
-    df['UP'] = (df['MÁXIMO'] - df['ABERTURA'])*100/df['ABERTURA']
-    df['DOWN'] = (df['ABERTURA'] - df['MÍNIMO'])*100/df['ABERTURA']
+    symbol = yf.Ticker(acao)
+    df = msft.history()
+    #df = pd.read_csv(acao+'.csv', decimal=',')
+    df['UP'] = (df['High'] - df['Open'])*100/df['Open']
+    df['DOWN'] = (df['Open'] - df['Lor'])*100/df['Open']
     ABERTURA = float(abertura)
     
     ordem_v = ('{"ordem": "venda",')+('"75%": "R${}",'.format((ABERTURA*(1 + df.describe()['UP']['25%']/100)).round(2)))+('"50%": "R${}",'.format((ABERTURA*(1 + df.describe()['UP']['50%']/100)).round(2)))+('"25%": "R${}"'.format((ABERTURA*(1 + df.describe()['UP']['75%']/100)).round(2)))+"}"
